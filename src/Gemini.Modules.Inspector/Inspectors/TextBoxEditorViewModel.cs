045fc66c0e00ef5b565e193abd776cb42c1dcd76
Luferau <luferau@mwmlab.com>
2018-02-16 20:21:57 +0200

    Completion of Inspector TextBoxEditor data entry by Enter pressing

--- src/Gemini.Modules.Inspector/Inspectors/TextBoxEditorViewModel.cs
+++ src/Gemini.Modules.Inspector/Inspectors/TextBoxEditorViewModel.cs
@@ -1,7 +1,15 @@
-﻿namespace Gemini.Modules.Inspector.Inspectors
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Gemini.Modules.Inspector.Inspectors
 {
     public class TextBoxEditorViewModel<T> : EditorBase<T>, ILabelledInspector
     {
-        
+        public void ValueChanged(Key key, TextBox textBox)
+        {
+            if (key != Key.Enter) return;
+
+            textBox?.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
+        }
     }
 }
